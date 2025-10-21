96374bb659afd4006f9033865593f915a74044cf
Joachim Ungar <joachim.ungar@gmail.com>
2025-10-21 14:34:07 +0200

    add tests for first and second bbox, intersects and ids

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,7 @@
 import json
 
 import pytest
+from shapely.geometry import box, mapping
 
 
 def test_app(client):
@@ -179,6 +180,166 @@ def test_pair_search_response(client, method: str, response_type: str):
         # make sure pairs do not contain identical items
         for first, second in feature_pairs:
             assert first != second
+
+
+# first-bbox, second-bbox
+@pytest.mark.parametrize("method", ["get", "post"])
+@pytest.mark.parametrize("first_bbox", [None, [6.21203, 43.36210, 6.35085, 43.48009]])
+@pytest.mark.parametrize("second_bbox", [None, [6.21203, 43.36210, 6.35085, 43.48009]])
+def test_bbox(client, method: str, first_bbox, second_bbox):
+    # bbox only intersects with product ASA_IMS_1PNESA20100602_094953_000000152090_00022_43162_0000
+    intersecting_id = "ASA_IMS_1PNESA20100602_094953_000000152090_00022_43162_0000"
+    params = {
+        "first-collections": ["ENVISAT.ASA.IMS_1P"],
+        "second-collections": ["ENVISAT.ASA.IMS_1P"],
+    }
+    if first_bbox:
+        params["first-bbox"] = ",".join(map(str, first_bbox))
+    if second_bbox:
+        params["second-bbox"] = ",".join(map(str, second_bbox))
+
+    response = client.request(
+        method=method,
+        url="/pair-search",
+        params=params if method == "get" else None,
+        content=json.dumps(params) if method == "post" else None,
+    )
+    if response.status_code != 200:
+        raise ValueError(f"Response: {response.status_code}, {response.text}")
+
+    response_json = response.json()
+
+    assert "numberPairsReturned" in response_json
+    assert "numberPairsMatched" in response_json
+
+    # special case: if both bboxes are set, no pairs are returned, because there is
+    # only one product
+    if first_bbox and second_bbox:
+        assert response_json["numberPairsReturned"] == 0
+        assert response_json["numberPairsMatched"] == 0
+        assert not response_json["featurePairs"]
+    else:
+        assert response_json["numberPairsReturned"] > 0
+        assert response_json["numberPairsMatched"] > 0
+        assert response_json["featurePairs"]
+        for first, second in response_json["featurePairs"]:
+            if first_bbox:
+                assert first == intersecting_id
+            if second_bbox:
+                assert second == intersecting_id
+
+
+# first-intersects, second-intersects
+@pytest.mark.parametrize("method", ["get", "post"])
+@pytest.mark.parametrize(
+    "first_intersects", [None, mapping(box(6.21203, 43.36210, 6.35085, 43.48009))]
+)
+@pytest.mark.parametrize(
+    "second_intersects", [None, mapping(box(6.21203, 43.36210, 6.35085, 43.48009))]
+)
+def test_intersects(client, method: str, first_intersects, second_intersects):
+    # bbox only intersects with product ASA_IMS_1PNESA20100602_094953_000000152090_00022_43162_0000
+    intersecting_id = "ASA_IMS_1PNESA20100602_094953_000000152090_00022_43162_0000"
+    params = {
+        "first-collections": ["ENVISAT.ASA.IMS_1P"],
+        "second-collections": ["ENVISAT.ASA.IMS_1P"],
+    }
+    if first_intersects:
+        params["first-intersects"] = json.dumps(first_intersects, separators=(",", ":"))
+    if second_intersects:
+        params["second-intersects"] = json.dumps(
+            second_intersects, separators=(",", ":")
+        )
+
+    response = client.request(
+        method=method,
+        url="/pair-search",
+        params=params if method == "get" else None,
+        content=json.dumps(params) if method == "post" else None,
+    )
+    if response.status_code != 200:
+        raise ValueError(f"Response: {response.status_code}, {response.text}")
+
+    response_json = response.json()
+
+    assert "numberPairsReturned" in response_json
+    assert "numberPairsMatched" in response_json
+
+    # special case: if both bboxes are set, no pairs are returned, because there is
+    # only one product
+    if first_intersects and second_intersects:
+        assert response_json["numberPairsReturned"] == 0
+        assert response_json["numberPairsMatched"] == 0
+        assert not response_json["featurePairs"]
+    else:
+        assert response_json["numberPairsReturned"] > 0
+        assert response_json["numberPairsMatched"] > 0
+        assert response_json["featurePairs"]
+        for first, second in response_json["featurePairs"]:
+            if first_intersects:
+                assert first == intersecting_id
+            if second_intersects:
+                assert second == intersecting_id
+
+
+# first-ids, second-ids
+@pytest.mark.parametrize("method", ["get", "post"])
+@pytest.mark.parametrize(
+    "first_ids",
+    [
+        None,
+        "ASA_IMS_1PNESA20100602_094953_000000152090_00022_43162_0000",
+        ["ASA_IMS_1PNESA20100602_094953_000000152090_00022_43162_0000"],
+    ],
+)
+@pytest.mark.parametrize(
+    "second_ids",
+    [
+        None,
+        "ASA_IMS_1PNESA20100602_094953_000000152090_00022_43162_0000",
+        ["ASA_IMS_1PNESA20100602_094953_000000152090_00022_43162_0000"],
+    ],
+)
+def test_ids(client, method: str, first_ids, second_ids):
+    intersecting_id = "ASA_IMS_1PNESA20100602_094953_000000152090_00022_43162_0000"
+    params = {
+        "first-collections": ["ENVISAT.ASA.IMS_1P"],
+        "second-collections": ["ENVISAT.ASA.IMS_1P"],
+    }
+    if first_ids:
+        params["first-ids"] = first_ids
+    if second_ids:
+        params["second-ids"] = second_ids
+
+    response = client.request(
+        method=method,
+        url="/pair-search",
+        params=params if method == "get" else None,
+        content=json.dumps(params) if method == "post" else None,
+    )
+    if response.status_code != 200:
+        raise ValueError(f"Response: {response.status_code}, {response.text}")
+
+    response_json = response.json()
+
+    assert "numberPairsReturned" in response_json
+    assert "numberPairsMatched" in response_json
+
+    # special case: if both bboxes are set, no pairs are returned, because there is
+    # only one product
+    if first_ids and second_ids:
+        assert response_json["numberPairsReturned"] == 0
+        assert response_json["numberPairsMatched"] == 0
+        assert not response_json["featurePairs"]
+    else:
+        assert response_json["numberPairsReturned"] > 0
+        assert response_json["numberPairsMatched"] > 0
+        assert response_json["featurePairs"]
+        for first, second in response_json["featurePairs"]:
+            if first_ids:
+                assert first == intersecting_id
+            if second_ids:
+                assert second == intersecting_id
 
 
 # https://pair-search-demo.eox.at/catalogue/pair-search?
